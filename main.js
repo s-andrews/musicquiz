6c2577ebfad2416ae0a1bd510bbb5dda22bb161e
Simon Andrews <simon.andrews@babraham.ac.uk>
2020-03-22 08:09:57 +0000

    Added the basic structure

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    // Load in the clips and place them in the document
+  
+
+    // Make the suggestions work
+
+
+    // Make the answers button work
+
+
+  }); 
