7402bfc647b1de742d70ddde218ba8b870f5810c
Simon Andrews <simon.andrews@babraham.ac.uk>
2020-03-22 08:21:02 +0000

    Added local JS link and read clips JSON

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
     // Load in the clips and place them in the document
-  
+    $.getJSON('clips.json', function(clips) {
+        //clips is the JSON string
+        for (i=0; i < clips.length; i++) {
+            console.log(clips[i]["file"])
+        }
+        
+
+    });
 
     // Make the suggestions work
 
