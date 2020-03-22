b5a76e7ea8d8636ffc07902463a03efe15f676f5
Simon Andrews <simon.andrews@babraham.ac.uk>
2020-03-22 09:51:36 +0000

    Populate the clips

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
     // Load in the clips and place them in the document
     $.getJSON('clips.json', function(clips) {
         //clips is the JSON string
+        clipdiv = $("#clips")
+
         for (i=0; i < clips.length; i++) {
-            console.log(clips[i]["file"])
+            clipdiv.append("<div class=\"clip text-center\">Clip "+(i+1)+": <audio controls><source src=\""+clips[i]["file"]+"\" type=\"audio/mp3\"></audio> Answer: <input type=\"text\" size=\"40\"></div>")
         }
         
 
