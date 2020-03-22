82851b70da3993aedd722d8182b946d80c653f96
Simon Andrews <simon.andrews@babraham.ac.uk>
2020-03-22 10:59:55 +0000

    Styled answers.  Added button

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
 
+
     // Load in the clips and place them in the document
     $.getJSON('clips.json', function(clips) {
         //clips is the JSON string
         clipdiv = $("#clips")
 
         for (i=0; i < clips.length; i++) {
-            clipdiv.append("<div class=\"clip text-center\">Clip "+(i+1)+": <audio controls><source src=\""+clips[i]["file"]+"\" type=\"audio/mp3\"></audio> Answer: <input type=\"text\" size=\"40\"></div>")
+            clipdiv.append("<div class=\"clip text-center\">Clip "+(i+1)+": <audio controls><source src=\""+clips[i]["file"]+"\" type=\"audio/mp3\"></audio> Answer: <select class=\"answers\"></select></div>")
         }
         
+
+        $.getJSON('suggestions.json', function(suggestions) {
+
+            allanswers = $(".answers")
+            allanswers.append("<option value=\"\"></option>")
+
+            for (i=0; i < suggestions.length; i++) {
+                allanswers.append("<option value=\""+suggestions[i]["index"]+"\">"+suggestions[i]["name"]+"</option>")
+            }
+            
+    
+            allanswers.select2({width: "10em"})
+        });
+    
 
     });
 
